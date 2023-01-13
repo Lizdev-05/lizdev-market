da9ce5c9a0b8376b58b9e77adb9aea555295fe5e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-13 10:22:42 +0100

    Imported Home, contact, header, footer as default and with routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Footer, Header } from './components'
+import { Home, Contact } from './pages'
 
 const App = () => {
   return (
-    <div>I love Jesus</div>
+    <>
+    <Header />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home />} />
+      <Route path='contact' element={ <Contact />} />
+
+    </Routes>
+    </BrowserRouter>
+    <Footer />
+    </>
   )
 }
 
