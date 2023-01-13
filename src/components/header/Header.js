ed460f462b9df3ad6909868c28b231018af08cd7
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-13 12:38:18 +0100

    Add the right side nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { logo } from "./Logo";
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa"
 
 const Header = () => {
   return (
-    <div>
+   
       <header>
-        <div className="styles.header">
+        <div className={styles.header}>
           {logo}
           <nav>
-            <ul></ul>
-            <div></div>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
+            <div className={styles["header-right"]}>
+              <span className={styles.links}>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/order-history'>My orders</Link>
+              </span>
+            </div>
           </nav>
         </div>
       </header>
-    </div>
+
   );
 };
 
--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 export const logo = (
-    <div className="styles.logo">
+    <div className={styles.logo}>
       <Link to="/">
         <h2>
           lizdev<span>mall</span>{" "}
