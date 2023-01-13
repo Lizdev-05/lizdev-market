d564ec8f90aeaee761465637ee9d6d95ad2f385d
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-13 11:30:29 +0100

    Add logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+const logo = (
+  <div className='styles.logo'>
+    <Link to='/'>
+   <h2 >lizdev<span>mall</span> </h2>
+  </Link>
+  </div>
+  
+)
+
   return (
-    <div>Header</div>
+    <div>
+      <header>
+       {logo}
+       <nav>
+        <ul>
+
+        </ul>
+        <div>
+          
+        </div>
+       </nav>
+
+      </header>
+    </div>
   )
 }
 
